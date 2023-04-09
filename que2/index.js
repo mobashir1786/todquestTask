51f57eae20f143c0472609d7410ae26931b087a8
Md Mobashir <mdmobashir786@gmail.com>
2023-04-10 01:42:44 +0530

    task completed

--- que2/index.js
+++ que2/index.js
@@ -1,10 +1,47 @@
-const expres =require("express");
-const app=expres();
-
-app.get('/',(req,res)=>{
-    res.send("Hello world");
+var WorksnapsTimeEntry = BaseSchema.extend({
+    student: {
+        type: Schema.ObjectId,
+        ref: 'Student'
+    },
+    timeEntry: {
+        type: Object
+    }
+});
+var StudentSchema = BaseSchema.extend({
+    firstName: {
+        type: String,
+        trim: true,
+        default: ''
+        // validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        default: ''
+        // validate: [validateLocalStrategyProperty, 'Please fill in your last name']
+    },
+    displayName: {
+        type: String,
+        trim: true
+    },
+    municipality: {
+        type: String
+    }
 });
 
-app.listen(4000,()=>{
-    console.log("http://localhost:4000");
-});
+Student.find()
+    .populate('times')
+    .exec((err, students) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        students.forEach((student) => {
+            console.log(`Student: ${student.firstName} ${student.lastName}`);
+            console.log(`Time entries:`);
+            student.times.forEach((timeEntry) => {
+                console.log(`${JSON.stringify(timeEntry)}`);
+            });
+        });
+    });
